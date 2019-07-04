a4207040cbd20cec48142dfb06b6a58a90873d9f
Alder Maven <43149194+AlderMaven@users.noreply.github.com>
2019-07-03 19:45:45 -0500

    initial code commit

---
+++ maxHeap.cpp
@@ -0,0 +1,48 @@
+//Created by Brandon Barnes
+
+#include <iostream>
+
+using namespace std;
+
+void heapify(int array[], int size, int index){
+	int max = index;
+	int l = 2*max+1;
+	int r = 2*max+2;
+	
+	if(l<size&&array[l]>array[max]){
+		max = l;
+	}
+	if(r<size&&array[r]>array[max]){
+		max = r;
+	}
+	if(max != index){
+		int temp = array[index];
+		array[index] = array[max];
+		array[max] = temp;
+		
+		heapify(array, size, max);
+	}
+	
+}
+
+void buildHeap(int array[], int size){
+	int startIndex = (size/2)-1;
+	
+	for(int i = startIndex; i>(-1);i--){
+		heapify(array, size, i);
+	}
+}
+
+
+int main(){
+	int array[] = {4, 10, 3, 5, 1};
+	
+	buildHeap(array, 5);
+	for(int i = 0; i<5; i++){
+		cout << array[i] << ' ' << endl;
+	}
+	
+	
+	
+	return 0;
+}
